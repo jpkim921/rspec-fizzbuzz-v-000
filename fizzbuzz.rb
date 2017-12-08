628c0a8684f32e9f2bea17377d96349cf117b9dc
jpkim921 <jpkim921@gmail.com>
2017-12-08 03:33:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
 require 'pry'
 
-def fizzbuzz
+def fizzbuzz(num)
+  if num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  elsif num % 15 == 0
+    return "FizzBuzz"
+  end
 
 binding.pry
 end
